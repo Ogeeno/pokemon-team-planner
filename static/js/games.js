be6849a35f0c84c5c244ecd2a92580466e9fcbe2
Ogeeno <ryan.ogino@gmail.com>
2025-06-03 15:27:03 -0700

    Update games.js

--- static/js/games.js
+++ static/js/games.js
@@ -214,19 +214,7 @@ export default {
     "rom": {
         "name": "Modern Indigo ROM",
         "gen": 9,
-        "dex_slugs": [
-            "kanto_home",
-            "johto_home",
-            "hoenn_home",
-            "sinnoh_home",
-            "unova_home",
-            "kalos_home",
-            "alola_home",
-            "galar_home",
-            "hisui_home",
-            "paldea_home",
-            "other_home"
-        ],
+        "dex_slugs": ["indigo_rom"],
         "mega": true,
         "gmax": true,
         "tera": true,
